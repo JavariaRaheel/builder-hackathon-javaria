b7c6c6e2286bc80a79639562bf7a81451dd8f92f
bintemukhtar1803@gmail.com <bintemukhtar1803@gmail.com>
2025-02-05 17:20:05 +0500

    new

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,11 +2,11 @@
 
 import {visionTool} from '@sanity/vision'
 import {defineConfig} from 'sanity'
-// import {structureTool} from 'sanity/structure'
+import {structureTool} from 'sanity/structure'
 
 import {apiVersion, dataset, projectId} from './src/sanity/env'
 import {schema} from './src/sanity/schemaTypes'
-// import {structure} from './src/sanity/structure'  // Ensure this import path is correct
+import {structure} from './src/sanity/structure'  // Ensure this import path is correct
 
 export default defineConfig({
   basePath: '/studio',
@@ -14,7 +14,7 @@ export default defineConfig({
   dataset,
   schema,
   plugins: [
-    // structureTool({structure}), // Pass structure function directly
+    structureTool({structure}), // Pass structure function directly
     visionTool({defaultApiVersion: apiVersion}),
   ],
 })
